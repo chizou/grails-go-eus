6e01f62730e66ef154e9554f98324e0a6d58d1e5
EC2 Default User <ec2-user@ip-172-31-17-253.us-west-2.compute.internal>
2016-09-13 11:42:43 +0000

    Move external user service back to 8077 and test with hardcoded IPs+
    52.88.226.0:8076 with eus running on :8077/external defined in find-eus.go ==> green dots :: yes
    52.88.226.0:8076 with eus running twice on docker cluster on :8077 (as jquery, not yet as a standalone eus) defined in find-eus.go ==> green/blue dots

--- src/eus/eus.go
+++ src/eus/eus.go
@@ -58,6 +58,6 @@ func main() {
 //        mux.HandleFunc("/work", work)
 //        mux.HandleFunc("/status", status)
         mux.HandleFunc("/external", simulatedExternalUserService)
-        http.ListenAndServe(":8076", mux)
+        http.ListenAndServe(":8077", mux)
 }
 
